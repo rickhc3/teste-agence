b2c195f3ec666f50f63d262f59907934ed355552
Ricardo Arantes <riarantes@uolinc.com>
2023-08-11 12:39:49 -0300

    test api in vercel

--- front/pages/index.vue
+++ front/pages/index.vue
@@ -1,9 +1,32 @@
 <template>
-  <Tutorial/>
+  <div>
+    <h1>Customers</h1>
+    <ul>
+      <li v-for="customer in customers" :key="customer.co_cliente">
+        {{ customer.no_fantasia }}
+      </li>
+    </ul>
+  </div>
 </template>
 
 <script>
+
 export default {
-  name: 'IndexPage'
+  name: 'IndexPage',
+  data() {
+    return {
+      customers: [],
+    };
+  },
+  mounted() {
+    this.fetchApi();
+  },
+  methods: {
+    fetchApi() {
+      this.$axios.get('customers').then((res) => {
+        this.customers = res.data.data;
+      });
+    },
+  },
 }
 </script>
